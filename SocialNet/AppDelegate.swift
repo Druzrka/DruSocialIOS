55051e3b702d3a6b3174a7481b3915454685fb6d
Druzrka <zazman1999@gmail.com>
2018-03-22 21:41:40 +0200

    initial commit

---
+++ SocialNet/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SocialNet
+//
+//  Created by Захар on 22.03.18.
+//  Copyright © 2018 Захар. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
+
---
+++ SocialNet/View/SignInVC.swift
@@ -0,0 +1,18 @@
+//
+//  SignInVC.swift
+//  SocialNet
+//
+//  Created by Захар on 22.03.18.
+//  Copyright © 2018 Захар. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
