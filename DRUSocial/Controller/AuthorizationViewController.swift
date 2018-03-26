6cdf9a4dd05e60e1ad007699c07b4bd1bc04c858
Druzrka <zazman1999@gmail.com>
2018-03-26 22:32:28 +0300

    authorization controller was added and some ui corrections

---
+++ DRUSocial/Controller/AuthorizationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AuthorizationViewController.swift
+//  DRUsocial
+//
+//  Created by Захар on 26.03.18.
+//  Copyright © 2018 Захар. All rights reserved.
+//
+
+import UIKit
+
+class AuthorizationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
